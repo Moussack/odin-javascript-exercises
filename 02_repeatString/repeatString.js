70782a835e00cb01186d2f003e33b28a65c13072
reyhan <reywahyono@gmail.com>
2022-07-25 13:00:38 +0700

    completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,23 @@
-const repeatString = function () {};
+const repeatString = function (theString, numOfWord) {
+   // variable below holds the string before doing the loop
+   let result = theString;
+
+   // if the word is 0 OR the string is EMPTY this IF will return an empty string
+   if (numOfWord === 0 || theString === '') return '';
+
+   // if the word is less than 0 this IF will return ERROR string
+   if (numOfWord < 0) return 'ERROR';
+
+   // ELSE it will run this loop and then return the result
+   for (let i = 1; i < numOfWord; i++) {
+      result = result + theString;
+   }
+
+   return result;
+};
+
+// const res = repeatString('', 10);
+// console.log(res);
 
 // Do not edit below this line
 module.exports = repeatString;
