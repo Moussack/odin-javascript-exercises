a342b9b0f56477c4062c94796971f88ceff66b51
reyhan <reywahyono@gmail.com>
2022-07-27 13:38:10 +0700

    completed 02 reverses single word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,12 +1,19 @@
-const reverseString = function () {
-   console.log('asdasd');
-   // split the string first
-   // the result will be an array
-   // reverse the array (find th method to reverse the array)
-   // join the array
+const reverseString = function (theString) {
+   console.log(`${theString} from original string`);
+   // split the string
+   const splittedString = theString.split('');
+
+   // the result will be an array and then reverse the array
+   const reversedArray = splittedString.reverse();
+
+   // join the array to build the string
+   const reversedString = reversedArray.join('');
+
+   // and return the reversed string
+   return reversedString;
 };
 
-reverseString();
+//console.log(reverseString('Hello'));
 
 // Do not edit below this line
 module.exports = reverseString;
