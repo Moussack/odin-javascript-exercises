e16be05b56fd4f501e22e23bb79095f935667d0b
reyhan <reywahyono@gmail.com>
2022-07-27 13:19:39 +0700

    first commit

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function () {
+   console.log('asdasd');
+   // split the string first
+   // the result will be an array
+   // reverse the array (find th method to reverse the array)
+   // join the array
 };
+
+reverseString();
 
 // Do not edit below this line
 module.exports = reverseString;
