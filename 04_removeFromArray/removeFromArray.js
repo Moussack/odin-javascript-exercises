633415f005d9646b39e18b8f46257c450e5de501
reyhan <reywahyono@gmail.com>
2022-07-28 14:59:42 +0700

    completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,13 @@
-const removeFromArray = function (arrayOfNums, item) {
+const removeFromArray = function (arrayOfNums, ...items) {
    // find the method to delete the array based on the item to remove
-   const result = arrayOfNums.filter((num) => num !== item);
+   // using rest operator so the function argument can have multiple arguments
+   itemToDelete = items;
+   const result = arrayOfNums.filter((num) => !itemToDelete.includes(num));
 
    return result;
 };
 
-console.log(removeFromArray([1, 2, 3, 4], 3));
+//console.log(removeFromArray([1, 2, 3, 4], 2, 6, 7));
 
 // Do not edit below this line
 module.exports = removeFromArray;
