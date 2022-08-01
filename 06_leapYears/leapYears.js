2d03f7d4a10467049b4d34c05e724fd195374715
reyhan <reywahyono@gmail.com>
2022-08-01 14:54:43 +0700

    completed 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (years) {
+   // determine the year that can divided by 4, 100 & 400 using modulo operation
+   if ((years % 4 === 0 && years % 100 !== 0) || years % 400 === 0) {
+      return true;
+   } else {
+      return false;
+   }
 };
+
+//console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
