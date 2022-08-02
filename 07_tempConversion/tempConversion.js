a65d0f981d8a18254c2949f02c5265703a461756
reyhan <reywahyono@gmail.com>
2022-08-02 14:02:00 +0700

    completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,18 @@
-const ftoc = function() {
-
+const ftoc = function (ftemp) {
+   const celcius = (ftemp - 32) * 0.5556;
+   return +celcius.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function (ctemp) {
+   const fahrenheit = ctemp * 1.8 + 32;
+   return +fahrenheit.toFixed(1);
 };
+
+console.log(ftoc(-100));
+console.log(ctof(100));
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+   ftoc,
+   ctof,
 };
