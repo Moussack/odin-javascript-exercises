858752c5115507f1356b3a31d9d3c87f92087e91
reyhan <reywahyono@gmail.com>
2022-10-21 16:57:07 +0700

    upto multiply

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,39 @@
-const add = function() {
-	
+const add = function (a, b) {
+   return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+   return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+   /*    let result = 0;
+   nums.forEach((num) => {
+      result = result + num;
+   });
+   return result; */
+
+   // Using reduce
+   let result = nums.reduce((prev, cur) => prev + cur, 0);
+   return result;
 };
 
-const multiply = function() {
-
+const multiply = function (nums) {
+   // Using reduce
+   let result = nums.reduce((prev, cur) => prev * cur);
+   return result;
 };
 
-const power = function() {
-	
-};
+const power = function () {};
 
-const factorial = function() {
-	
-};
+const factorial = function () {};
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+   add,
+   subtract,
+   sum,
+   multiply,
+   power,
+   factorial,
 };
