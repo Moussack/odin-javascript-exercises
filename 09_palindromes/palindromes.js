a0b624cf38661a0ec8872610ca510e8fa1335d71
reyhan <reywahyono@gmail.com>
2022-10-24 14:53:26 +0700

    completed palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (str) {
+   const word = str
+      .trim()
+      .toLowerCase()
+      .replace(/[&\/\\#,+()$~%.!'":*?<>{} ]/g, '');
 
+   const reversed = str
+      .split('')
+      .reverse()
+      .join('')
+      .trim()
+      .toLowerCase()
+      .replace(/[&\/\\#,+()$~%.!'":*?<>{} ]/g, '');
+
+   console.log(word, reversed);
+
+   if (word === reversed) return true;
+   return false;
 };
 
 // Do not edit below this line
