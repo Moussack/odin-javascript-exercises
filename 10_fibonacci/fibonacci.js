f86f44a79271c38ebb4f3b000d4cb9a934b342af
reyhan <reywahyono@gmail.com>
2022-10-26 12:16:30 +0700

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+   let seq = [0, 1]; // initialize fibonacci sequence first
+   for (let i = 2; i < +num + 1; i++) {
+      seq[i] = seq[i - 2] + seq[i - 1];
+   }
+   //console.log(seq);
+   if (num < 0) return `OOPS`;
+   return seq[seq.length - 1];
 };
 
 // Do not edit below this line
